7bd81f8439e92a514b8c4da11159dbcac19d3425
Joelljw <15056194@myrp.edu.sg>
2017-05-30 12:01:23 +0800

    Notification Completed

--- app/src/main/java/com/example/a15056194/demonotification/MainActivity.java
+++ app/src/main/java/com/example/a15056194/demonotification/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnNotify1 = (Button) findViewById(R.id.btnNotify1);
+        btnNotify2 = (Button) findViewById(R.id.btnNotify2);
 
         btnNotify1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +46,34 @@ public class MainActivity extends AppCompatActivity {
 
                 //Set Priority
                 builder.setPriority(Notification.PRIORITY_HIGH);
+
+                Notification n = builder.build();
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+                notificationManager.notify(notificationID, n);
+                finish();
+            }
+        });
+
+        btnNotify2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+
+                PendingIntent pIntent = PendingIntent.getActivity(MainActivity.this, requestCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                Notification.BigTextStyle bigText = new Notification.BigTextStyle();
+                bigText.bigText("This is one big text");
+                bigText.setBigContentTitle("Big Text - Long Content");
+                bigText.setSummaryText("Reflection Journal?");
+
+                Notification.Builder builder = new Notification.Builder(MainActivity.this);
+                builder.setContentTitle("Amazing Offer!");
+                builder.setContentText("Subject");
+                builder.setSmallIcon(R.mipmap.ic_launcher);
+                builder.setContentIntent(pIntent);
+                builder.setStyle(bigText);
+                builder.setAutoCancel(true);
 
                 Notification n = builder.build();
                 NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
